f8a22bc3fb48d310bc665ceb3b416f2531aafda4
jahtzee <ccp.violation@gmail.com>
2020-02-20 14:09:52 +0100

    added user interface

--- rps.js
+++ rps.js
@@ -8,46 +8,58 @@ const buttons = document.querySelectorAll('.btn');
 buttons.forEach(
 	button => {
 		button.addEventListener('click', (e) => {
-			playerSelection = button.getAttribute('data-value');
-			computerSelection = computerPlay();
-			let result = playRound(playerSelection, computerSelection);
-			scoreTracker(result);
-			resultAlert(result);
-			if (playerScore === 5 || computerScore === 5) {
-				announce();
-				playerSelection = '';
-				computerSelection = '';
-				playerScore = 0;
-				computerScore = 0;
+				main(button);
 			}
-		})
+		)
 	}
 )
+
+function main(button) {
+	turnOnScore();
+	turnOnInfo();
+	playerSelection = button.getAttribute('data-value');
+	computerSelection = computerPlay();
+	let result = playRound(playerSelection, computerSelection);
+	updateScoreVars(result);
+	updateScoreTracker();
+	resultAlert(result);
+	endGame();
+}
+
+function endGame() {
+	if (playerScore === 5 || computerScore === 5) {
+		announceWinner();
+		playerSelection = '';
+		computerSelection = '';
+		playerScore = 0;
+		computerScore = 0;
+	}
+}
 
 function playRound(playerSelection, computerSelection) {
   let logic = {
     rock: {
-      win: "scissors", 
-      loss: "paper"
+      win: 'scissors', 
+      loss: 'paper'
     },
     paper: {
-      win: "rock",
-      loss: "scissors"
+      win: 'rock',
+      loss: 'scissors'
     },
     scissors: {
-      win: "paper",
-      loss: "rock"
+      win: 'paper',
+      loss: 'rock'
     }          
   }
 
   if (logic[playerSelection].win === computerSelection) {
-    return "win";
+    return 'win';
   }
   else if (logic[playerSelection].loss === computerSelection) {
-    return "loss";
+    return 'loss';
   }
   else {
-    return "draw";
+    return 'draw';
   }
 }
 
@@ -55,11 +67,11 @@ function computerPlay() {
   let choice = getRandomInt(3);
   switch (choice) {
     case 0:
-      return "rock";
+      return 'rock';
     case 1:
-      return "paper";
+      return 'paper';
     case 2:
-      return "scissors";
+      return 'scissors';
   }
 }
 
@@ -67,25 +79,30 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-
-function announce() {
-  if (playerScore > computerScore) {
-    alert("You win the game!");
-  }
-  else if (playerScore < computerScore) {
-    alert("You lost the game!");
-  }
-  else {
-    alert("The game ended in a draw!");
-  }
+function turnOnScore() {
+	const h1 = document.querySelector('h1');
+	const score = document.querySelector('#score');
+	
+	if (score.getAttribute('class') == 'off') {
+		h1.setAttribute('class', 'off');
+		score.removeAttribute('class');
+	}
 }
 
-function scoreTracker(result) {
+function turnOffScore() {
+	const h1 = document.querySelector('h1');
+	const score = document.querySelector('#score');
+
+	score.setAttribute('class', 'off');
+	h1.removeAttribute('class');
+}
+
+function updateScoreVars(result) {
   switch (result) {
-    case "win":
+    case 'win':
       playerScore += 1;
       return;
-    case "loss":
+    case 'loss':
       computerScore += 1;
       return;
   }
@@ -94,16 +111,57 @@ function scoreTracker(result) {
   return;
 }
 
+function updateScoreTracker() {
+	const p_score = document.querySelector('#p-score');
+	const c_score = document.querySelector('#c-score');
+
+	p_score.textContent = playerScore;
+	c_score.textContent = computerScore;
+}
+
+function turnOnInfo() {
+	const info = document.querySelector('#info')
+
+	if (info.getAttribute('class') === 'off') {
+		info.removeAttribute('class');
+	}
+}
+
+function turnOffInfo() {
+	const info = document.querySelector('#info')
+
+	info.setAttribute('class', 'off');
+}
+
+function updateInfo(string) {
+	const info = document.querySelector('#info')
+	info.innerHTML = string;
+}
+
 function resultAlert(result) {
-  switch (result) {
-    case "win":
-      alert("You win!"+" "+playerSelection+" beats "+computerSelection+"!");
+	let p_choice = '<span style="color: #ff312e;">'+playerSelection.toUpperCase()+'</span>';
+	let c_choice = '<span style="color: #ff312e;">'+computerSelection.toUpperCase()+'</span>';
+
+	switch (result) {
+    case 'win':
+      updateInfo('You score!'+' '+p_choice+' beats '+c_choice+'!');
       break;
-    case "loss":
-      alert("You lose!"+" "+computerSelection+" beats "+playerSelection+"!");
+    case 'loss':
+      updateInfo('Computer scores!'+' '+c_choice+' beats '+p_choice+'!');
       break;
-    case "draw":
-      alert("Draw! The computer chose "+computerSelection+"!");
+    case 'draw':
+      updateInfo('Draw! The computer chose '+c_choice+'!');
   }
-  alert("Your current score: "+playerScore+"\nComputer score: "+computerScore);
+}
+
+function announceWinner() {
+  if (playerScore > computerScore) {
+    updateInfo('You <span style="color: #ff312e;">win</span> the game!<br>How about another round?');
+  }
+  else if (playerScore < computerScore) {
+    updateInfo('You <span style="color: #ff312e;">lost</span> the game!<br>Want to try again?');
+  }
+  else {
+    updateInfo('The game ended in a <span style="color: #ff312e;">draw</span>!<br>Want to try again?');
+  }
 }
